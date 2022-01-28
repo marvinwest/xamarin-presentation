60f854edd277113926d6234362d0230092456334
marvinwest <marvin.westphal@stud.uni-due.de>
2022-01-28 18:44:05 +0100

    Code from the presentation

--- app/presentation/presentation/MainPage.xaml.cs
+++ app/presentation/presentation/MainPage.xaml.cs
@@ -10,9 +10,43 @@ namespace presentation
 {
     public partial class MainPage : ContentPage
     {
+
+        private static String[] sentences =
+        {
+            "Hello Marvin!",
+            "Hello Tabea!",
+            "Hello Piush!"
+        };
+
+        String sentence;
+
         public MainPage()
         {
+            RandomizeSentence();
+            BindingContext = this;
             InitializeComponent();
+        }
+
+        void Shuffle_OnClicked(object sender, EventArgs e)
+        {
+            RandomizeSentence();
+        }
+
+        public String Sentence
+        {
+            get { return sentence;  }
+            set
+            {
+                sentence = value;
+                OnPropertyChanged("Sentence");
+            }
+        }
+
+        private void RandomizeSentence()
+        {
+            Random rand = new Random();
+            int index = rand.Next(sentences.Length);
+            Sentence = sentences[index];
         }
     }
 }
